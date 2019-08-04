83b410731f5f6bd6b7c1e9c6afeeba5e8b371512
TravisBassett <veristis@gmail.com>
2019-08-04 15:43:58 -0700

    build vue project with cli, added bootstrap-vue, started porting bootstrap from previous build 2/3 pages compiling properly

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 
 //put here, believing this to be my app entry point?
-import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
---
+++ src/views/SearchResults.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class="searchresults">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Search Results Page">
+    <meta name="author" content="Travis Bassett">
+
+    <title>Results Page</title>
+
+    <!-- Custom fonts for this theme -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
+        type="text/css">
+
+    <!-- Theme CSS -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container">
+        <div class="jumbotron bg-secondary">
+            <h1 class="display-1 text-center text-white">Results Page</h1>
+            <p class="text-center text-white">Information kindly provided by the OMBD API</p>
+        </div>
+
+        <div class="container">
+            <h2 class ="text-center">Search Results</h2>
+            <p class="text-center">You searched for ___________. Here are the results.</p>
+            <div class="card m-3">
+                <div class="card-body">
+                    <h4 class="card-title">Movie Result</h4>
+                    <p class="card-text">Some example text. Some example text.</p>
+                    <a href="#" class="card-link">Link to Result</a>
+                    <a href="#" class="card-link">Another possible link</a>
+                </div>
+            </div>
+            <div class="card m-3">
+                <div class="card-body">
+                    <h4 class="card-title">Movie Result</h4>
+                    <p class="card-text">Some example text. Some example text.</p>
+                    <a href="#" class="card-link">Link to Result</a>
+                    <a href="#" class="card-link">Another possible link</a>
+                </div>
+            </div>
+            <div class="card m-3">
+                <div class="card-body">
+                    <h4 class="card-title">Movie Result</h4>
+                    <p class="card-text">Some example text. Some example text.</p>
+                    <a href="#" class="card-link">Link to Result</a>
+                    <a href="#" class="card-link">Another possible link</a>
+                </div>
+            </div>
+            <div class="card m-3">
+                <div class="card-body">
+                    <h4 class="card-title">Movie Result</h4>
+                    <p class="card-text">Some example text. Some example text.</p>
+                    <a href="#" class="card-link">Link to Result</a>
+                    <a href="#" class="card-link">Another possible link</a>
+                </div>
+            </div>
+            <div class="card m-3">
+                <div class="card-body">
+                    <h4 class="card-title">Movie Result</h4>
+                    <p class="card-text">Some example text. Some example text.</p>
+                    <a href="#" class="card-link">Link to Result</a>
+                    <a href="#" class="card-link">Another possible link</a>
+                </div>
+            </div>
+            <div class="card m-3">
+                <div class="card-body">
+                    <h4 class="card-title">Movie Result</h4>
+                    <p class="card-text">Some example text. Some example text.</p>
+                    <a href="#" class="card-link">Link to Result</a>
+                    <a href="#" class="card-link">Another possible link</a>
+                </div>
+            </div>
+            <div class="card m-3">
+                <div class="card-body">
+                    <h4 class="card-title">Movie Result</h4>
+                    <p class="card-text">Some example text. Some example text.</p>
+                    <a href="#" class="card-link">Link to Result</a>
+                    <a href="#" class="card-link">Another possible link</a>
+                </div>
+            </div>
+        </div>
+
+
+        <footer>
+            <div class="jumbotron bg-secondary">
+                <p class="text-center text-white">Copyright Travis Bassett 2019</p>
+            </div>
+        </footer>
+    </div>
+
+
+</body>
+
+</div>
+    
+    <!--<img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>-->
+
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+
+
+
