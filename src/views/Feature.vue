0f33d8f31b94a21a6560c76d7c60d9112a037273
TravisBassett <veristis@gmail.com>
2019-08-25 17:10:30 -0700

    building project with upgrades to the style of both the search page and the feature page, nearing completion of both vues

--- src/views/Feature.vue
+++ src/views/Feature.vue
@@ -1,40 +1,41 @@
 <template>
   <div class="feature">
     <div class="container">
-    
-      
-    <div class="jumbotron bg-secondary">
-      <h1 class="display-1 text-center text-white">{{ results.Title }}</h1>
+      <div class="jumbotron bg-secondary">
+        <h1 class="display-1 text-center text-white">{{ results.Title }}</h1>
+        <p class="text-center text-white">Information kindly provided by the OMBD API</p>
+      </div>
+
       <p class="text-center text-white">Information kindly provided by the OMBD API</p>
-    </div>
+      
+      <div class="header1">
+      <h4>Plot</h4>
+      </div>
 
-    <p class="text-center text-white">Information kindly provided by the OMBD API</p>
-
-        <h4>Plot</h4>
-        {{ results.Plot }}
-
-        <div class="row">
-
-        <div class="col-sm-5"><img :src="results.Poster"></div>
+      {{ results.Plot }}
+      <div class="row">
+        <div class="col-sm-5">
+          <img :src="results.Poster" />
+        </div>
 
         <div class="col-sm-7">
-        <h4>Move Details:</h4>
-        <p>Year: {{results.Year}}</p>
-        <p>Rated: {{results.Rated}}</p>
-        <p>Released: {{results.Released}}</p>
-        <p>Runtime: {{results.Runtime}}</p>
-        <p>Genre: {{results.Genre}}</p>
-        <p>Director: {{results.Director}}</p>
+          <div class="header2">
+          <h4>Move Details:</h4>
+          </div>
+          <p>Year: {{results.Year}}</p>
+          <p>Rated: {{results.Rated}}</p>
+          <p>Released: {{results.Released}}</p>
+          <p>Runtime: {{results.Runtime}}</p>
+          <p>Genre: {{results.Genre}}</p>
+          <p>Director: {{results.Director}}</p>
         </div>
+      </div>
 
-        </div>
-
-              <div class="jumbotron bg-secondary">
+      <div class="jumbotron bg-secondary">
         <p class="text-center text-white">Copyright Travis Bassett 2019</p>
       </div>
-        
-        </div>
-</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -72,58 +73,30 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.drinksearch {
-  font-size: 1.4rem;
+h1,h4 {
+  padding-bottom: 1rem,
 }
-input[type="text"] {
-  border-top: none;
-  border-left: none;
-  border-right: none;
-  border-bottom: 1px solid #333;
-  width: 300px;
-  font-size: 1.4rem;
-  color: #2c3e50;
-  font-weight: 300;
-  background: rgba(0, 0, 0, 0.02);
-  padding: 0.5rem;
+.header1 {
+  background-color:cornflowerblue;
+  border: 2px solid;
+  border-radius: 6px;
 }
-button {
-  background: #333;
-  padding: 0.5rem;
-  font-weight: 300;
-  color: #fff;
-  border: none;
-  cursor: pointer;
-  font-size: 1.4rem;
+.header2 {
+  background-color: cornflowerblue;
+  border: 2px solid;
+  margin-top: 2rem;
+  border-radius: 6px;
+  
 }
-h1,
-h4 {
-  font-weight: normal;
+img {
+  border: 1.2px solid;
+  margin: 1rem;
+  border-radius: 6px;
 }
-ul.results {
-  list-style-type: none;
-  padding: 0;
+
+.container
+{
+  background-color:cornsilk
 }
-.results li {
-  display: inline-block;
-  margin: 10px;
-  border: solid 1px #333;
-  padding: 0.5rem;
-  width: 200px;
-  min-height: 100px;
-  color: #fff;
-  background: rgba(0, 0, 0, 0.7);
-}
-ul.errors {
-  list-style-type: none;
-}
-.errors li {
-  border: 1px solid red;
-  color: red;
-  padding: 0.5rem;
-  margin: 10px 0;
-}
-a {
-  color: #ce1818;
-}
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
-<div class="home">
-
-  <div class="container">
-    <div class="jumbotron bg-secondary">
-      <h1 class="display-1 text-center text-white">Search for a Movie</h1>
-      <p class="text-center text-white">Information kindly provided by the OMBD API</p>
-    </div>
+  <div class="home">
+    <div class="container">
+      <div class="jumbotron bg-secondary">
+        <h1 class="display-1 text-center text-white">Search for a Movie</h1>
+        <p class="text-center text-white">Information kindly provided by the OMBD API</p>
+      </div>
 
       <form v-on:submit.prevent="sendResult">
         <div class="form-group">
@@ -24,18 +23,16 @@
         </div>
         <button type="submit" class="btn btn-secondary m-3">Submit</button>
       </form>
-  
 
-  
       <div class="jumbotron bg-secondary">
         <p class="text-center text-white">Copyright Travis Bassett 2019</p>
       </div>
+    </div>
   </div>
-</div>
 
-<!--<img alt="Vue logo" src="../assets/logo.png">
+  <!--<img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-</div>-->
+  </div>-->
 </template>
 
 <script>
@@ -58,3 +55,33 @@ export default {
   }
 };
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+input {
+  width: 300px;
+  font-size: 1.4rem;
+  font-weight: 300;
+  background: rgba(0, 0, 0, 0.02);
+  padding: 0.5rem;
+  margin: auto;
+  border: 2p black solid;
+}
+
+.container
+{
+  background-color: cornsilk
+}
+
+button {
+  background: cornflowerblue;
+  padding: 0.5rem;
+  font-weight: 250;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  font-size: 1.4rem;
+}
+
+</style>
